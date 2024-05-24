94f9a277b2dca9d5355302197c23861e690dc8f0
Pierrolefou881 <dpierre394@gmail.com>
2024-05-24 16:55:59 +0200

    j'ai oublié d'éteindre le four

--- CatAclysm/Assets/Scripts/Data/Character/Talent.cs
+++ CatAclysm/Assets/Scripts/Data/Character/Talent.cs
@@ -13,15 +13,6 @@ namespace CatAclysm.Character
         [SerializeField]
         private string description;
 
-        public override void ComputePoints(Cat cat)
-        {
-            cat.TalentPoints = (int) Mathf.Pow(2, cat.Vibrisse);
-            if (cat.Vibrisse == 5) 
-            {
-                cat.TalentPoints -= 8;
-            }
-        }
-
         protected override int GetPointCapital(Cat cat) => cat.TalentPoints;
         protected override void ConsumePoints(Cat cat, int numberOfPoints) => cat.TalentPoints -= numberOfPoints;
         protected override void RefundPoints(Cat cat, int numberOfPoints) => cat.TalentPoints += numberOfPoints;
