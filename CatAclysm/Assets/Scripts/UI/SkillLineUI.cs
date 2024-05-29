28c4a7005e37457fb43af308ce4206f40c4e2d1e
Pierrolefou881 <dpierre394@gmail.com>
2024-05-29 13:57:31 +0200

    lignes de tableau compétences / talent ready

--- CatAclysm/Assets/Scripts/UI/SkillLineUI.cs
+++ CatAclysm/Assets/Scripts/UI/SkillLineUI.cs
@@ -2,7 +2,6 @@ using CatAclysm.Behavior;
 using CatAclysm.Character;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace CatAclysm.UI
 {
@@ -14,37 +13,19 @@ namespace CatAclysm.UI
         [SerializeField]
         private CatSkillExpositionBehavior skillExpositionBehavior;
 
-        public Skill Skill { get; set; }
-
-        private void OnEnable()
+        protected override void OnEnable()
         {
-            powerName.text = Skill.name;
-            basePoints.text = Skill.BaseSkill.ToString();
-            rank.text = Skill.Rank.ToString();
-            currentRank = Skill.Rank;
-            Skill.RankChanged += Skill_RankChanged;
-            CheckButtonsVisibilities();
-        }
-
-        private void OnDisable()
-        {
-            Skill.RankChanged -= Skill_RankChanged;
-        }
-
-        private void Skill_RankChanged(object sender, int e)
-        {
-            rank.text = e.ToString();
-            currentRank = e;
-            CheckButtonsVisibilities();
+            base.OnEnable();
+            basePoints.text = (Power as Skill).BaseSkill.ToString();
         }
 
         protected override void CheckButtonsVisibilities()
         {
             base.CheckButtonsVisibilities();
-            upArrow.gameObject.SetActive(skillExpositionBehavior.CanPurchaseSkillRank(currentRank + 1, Skill.name));
+            upArrow.gameObject.SetActive(skillExpositionBehavior.CanPurchaseSkillRank(currentRank + 1, Power.name));
         }
 
-        public override void PurchaseNextRank() => skillExpositionBehavior.SetSkillToRank(currentRank + 1, Skill.name);
-        public override void RestoreToPreviousRank() => skillExpositionBehavior.SetSkillToRank(currentRank - 1, Skill.name);
+        public override void PurchaseNextRank() => skillExpositionBehavior.SetSkillToRank(currentRank + 1, Power.name);
+        public override void RestoreToPreviousRank() => skillExpositionBehavior.SetSkillToRank(currentRank - 1, Power.name);
     }
 }
